2bf88ca608ec60fae43a1d41ef3521bde8704bea
Kate McKay <katemckay@codefforaustralia.org>
2019-04-02 12:15:39 +1100

    Images spacing

--- src/components/individualOrHousehold.vue
+++ src/components/individualOrHousehold.vue
@@ -5,15 +5,13 @@
   <div class="au-body">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-12 col-sm-6">
+				<div class="col-md-12">
 					<h2 class="text-center">Let's get started!</h2>
 					<h3>Are you an Individual or a Household?</h3>
 
 						<div id="radioButtons">
 							<div class="row">
-								<div class="col-md-12">
 									<br>
-
 										<div class="col-md-6">
 											<img class="icon" src="docs/assets/img/children.png" alt="Individual">
 												<label class="au-control-input au-control-input--small bottom-btn">
@@ -29,8 +27,6 @@
 													<span class="au-control-input__text">Household</span>
 												</label>
 									  </div><!-- end col -->
-
-								</div> <!-- end col -->
 							</div><!-- row -->
 						</div><!-- radio buttons -->
 
