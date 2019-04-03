46b8122e52aec4e2bae73d9ea1df707badf59d3b
Kate McKay <katemckay@codefforaustralia.org>
2019-04-03 17:02:19 +1100

    Added rebate images

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -6,7 +6,7 @@
         <div v-for="(rebate_obj, index) in rebatesList" :key="index" class="col-xs-4">
 
           <div class="card mb-4" style="width: 18rem;">
-            <img class="card-img-top img-fluid" src="docs/assets/img/600x260.jpg" alt="Card image cap">
+            <img class="card-img-top img-fluid" :src="rebate_obj.srcImage" :alt="rebate_obj.rebateName">
               <div class="card-body">
                 <h4 class="card-title">{{rebate_obj.rebateName}}</h4>
                 <p class="card-text">Content about rebate goes here. Perhaps the eligibility criteria.</p>
