15868171c0a0688a2c523797ca854e037e14571c
Kate McKay <katemckay@codefforaustralia.org>
2019-04-04 15:34:02 +1100

    Added gas rebate on card

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -5,13 +5,15 @@
       <div class="row">
         <div v-for="(rebate_obj, index) in rebatesList" :key="index" class="col-xs-4">
 
-          <div class="card mb-4" style="width: 18rem;">
+          <div class="card" style="width: 18rem;">
             <img class="card-img-top img-fluid" :src="rebate_obj.srcImage" :alt="rebate_obj.rebateName">
               <div class="card-body">
                 <h4 class="card-title">{{rebate_obj.rebateName}}</h4>
                 <p class="card-text">Content about rebate goes here. Perhaps the eligibility criteria.</p>
                 <br>
                 <a href="#" class="btn btn-primary">Link to Rebate website</a>
+                <br>
+                <br>
               </div><!-- card-body -->
           </div><!-- card -->
         </div><!-- end col -->
