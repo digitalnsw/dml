73cd6bf509a31386510318765bc2f7c5fb1a30e3
Damian Dzieduch <d.dzieduch@gmail.com>
2019-04-08 13:17:43 +1000

    Fix cards error

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -3,13 +3,64 @@
   <div id="result">
     <div class="container au-grid">
       <div class="row">
-        <div v-for="(rebate_obj, index) in rebatesList.filter(isVisible)" :key="index" class="col-xs-4">
-
+        <div class="col-xs-4">
           <div class="card" style="width: 18rem;">
-            <img class="card-img-top img-fluid" :src="rebate_obj.srcImage" :alt="rebate_obj.rebateName">
+            <img class="card-img-top img-fluid" src="" alt="">
               <div class="card-body">
-                <h4 class="card-title">{{rebate_obj.rebateName}}</h4>
-                <p class="card-text">Content about rebate goes here. Perhaps the eligibility criteria.</p>
+                <h4 class="card-title">Card name</h4>
+                <p class="card-text">Content about card goes here. Perhaps the eligibility criteria.</p>
+                <br>
+                <a href="#" class="btn btn-primary">Link to Rebate website</a>
+                <br>
+                <br>
+              </div><!-- card-body -->
+          </div><!-- card -->
+        </div><!-- end col -->
+        <div class="col-xs-4">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-fluid" src="" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Card name</h4>
+                <p class="card-text">Content about card goes here. Perhaps the eligibility criteria.</p>
+                <br>
+                <a href="#" class="btn btn-primary">Link to Rebate website</a>
+                <br>
+                <br>
+              </div><!-- card-body -->
+          </div><!-- card -->
+        </div><!-- end col -->
+        <div class="col-xs-4">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-fluid" src="" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Card name</h4>
+                <p class="card-text">Content about card goes here. Perhaps the eligibility criteria.</p>
+                <br>
+                <a href="#" class="btn btn-primary">Link to Rebate website</a>
+                <br>
+                <br>
+              </div><!-- card-body -->
+          </div><!-- card -->
+        </div><!-- end col -->
+        <div class="col-xs-4">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-fluid" src="" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Card name</h4>
+                <p class="card-text">Content about card goes here. Perhaps the eligibility criteria.</p>
+                <br>
+                <a href="#" class="btn btn-primary">Link to Rebate website</a>
+                <br>
+                <br>
+              </div><!-- card-body -->
+          </div><!-- card -->
+        </div><!-- end col -->
+        <div class="col-xs-4">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-fluid" src="" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Card name</h4>
+                <p class="card-text">Content about card goes here. Perhaps the eligibility criteria.</p>
                 <br>
                 <a href="#" class="btn btn-primary">Link to Rebate website</a>
                 <br>
