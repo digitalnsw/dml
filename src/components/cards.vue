6b499177b7a2cf2c6e43d6952c367daa3f69aa7c
Kate McKay <katemckay@codefforaustralia.org>
2019-04-10 14:34:58 +1000

    Changed radio to checkbox

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -6,13 +6,16 @@
         <h3>Do you have any of the following Pension, Concession or Membership cards?</h3>
         <!-- <div class="col-xs-4 AU-spacing"> -->
         <div v-for="(card_obj, index) in cardsList.filter(isVisible)" :key="index" class="col-xs-4">
-          <div class="card" style="width: 18rem; min-height: 20rem;">
+          <div class="card" style="">
             <img class="card-img-top img-fluid" :src="card_obj.srcImage" :alt="card_obj.cardName">
               <div class="card-body">
                 <h4 class="card-title">{{card_obj.cardName}}<br>{{card_obj.cardName2}}</h4>
                 <p class="card-text">{{card_obj.cardText}}</p>
                 <br>
-                <input type="radio" :value="card_obj.variable" name="cards">
+                <label class="au-control-input au-control-input--small" style="margin-top: 3rem;" >
+									<input class="au-control-input__input" name="cards" type="checkbox" :value="card_obj.variable">
+									<span class="au-control-input__text"></span>
+								</label>
                 <br>
                 <br>
               </div><!-- card-body -->
